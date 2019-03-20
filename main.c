64bfd95fe62e62892bc8da4ce28469ed6c23a49a
ViviTYW <48053581+ViviTYW@users.noreply.github.com>
2019-03-20 22:13:07 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,5 +1,28 @@
+nt oneint(int a)
+{
+	return a+a;
+}
+
+int manyint(int a, int b, int c, int d, int e, int f)
+{
+	return a+b+c+d+e;
+}
+
+void manyreturn(int a, int b, int *add, int *sub, int *mul, int *divi, int *square) 
+{
+	*add = a+b ;
+	*sub = a-b ;
+	*mul = a*b ;
+	*divi = a/b ;
+	*square =a^b;
+}
+
 void reset_handler(void)
 {
+	int add, sub, mul, divi, square ;
+	oneint(1);
+	manyint(1, 2, 3, 4, 5, 6);
+	manyreturn(1, 2, &add, &sub, &mul, &divi, &square);
 	while (1)
 		;
 }
